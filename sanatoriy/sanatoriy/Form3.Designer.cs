0a8fcca5719cfb10d5fb71300b160a4d2bc98907
EgorNa <egrghost15@gmail.com>
2022-12-26 01:03:13 +0400

    Добавил форму самой услуги

--- sanatoriy/sanatoriy/Form3.Designer.cs
+++ sanatoriy/sanatoriy/Form3.Designer.cs
@@ -29,39 +29,39 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.servicesPanel = new System.Windows.Forms.Panel();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.profileButton = new System.Windows.Forms.Button();
             this.offButton = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.nameInfoBox = new System.Windows.Forms.TextBox();
             this.panel3 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fullNameLabel = new System.Windows.Forms.Label();
+            this.roleLabel = new System.Windows.Forms.Label();
+            this.servicesPanel.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // servicesPanel
             // 
-            this.panel1.AutoScroll = true;
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(1, 170);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1279, 538);
-            this.panel1.TabIndex = 0;
+            this.servicesPanel.AutoScroll = true;
+            this.servicesPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.servicesPanel.Controls.Add(this.button3);
+            this.servicesPanel.Controls.Add(this.button2);
+            this.servicesPanel.Controls.Add(this.button1);
+            this.servicesPanel.Location = new System.Drawing.Point(1, 170);
+            this.servicesPanel.Name = "servicesPanel";
+            this.servicesPanel.Size = new System.Drawing.Size(1279, 538);
+            this.servicesPanel.TabIndex = 0;
             // 
             // button3
             // 
@@ -94,10 +94,10 @@
             // 
             this.panel2.BackColor = System.Drawing.Color.White;
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.roleLabel);
+            this.panel2.Controls.Add(this.fullNameLabel);
             this.panel2.Controls.Add(this.profileButton);
             this.panel2.Controls.Add(this.offButton);
-            this.panel2.Controls.Add(this.textBox1);
-            this.panel2.Controls.Add(this.nameInfoBox);
             this.panel2.Controls.Add(this.panel3);
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
@@ -129,39 +129,6 @@
             this.offButton.TabIndex = 25;
             this.offButton.Text = "Выход";
             this.offButton.UseVisualStyleBackColor = false;
-            // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.Color.White;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Enabled = false;
-            this.textBox1.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(406, 47);
-            this.textBox1.MaxLength = 100;
-            this.textBox1.MinimumSize = new System.Drawing.Size(150, 30);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.PlaceholderText = "\"Здесь должна показываться роль пользователя\"";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(446, 30);
-            this.textBox1.TabIndex = 24;
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // nameInfoBox
-            // 
-            this.nameInfoBox.BackColor = System.Drawing.Color.White;
-            this.nameInfoBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.nameInfoBox.Enabled = false;
-            this.nameInfoBox.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.nameInfoBox.Location = new System.Drawing.Point(406, 11);
-            this.nameInfoBox.MaxLength = 100;
-            this.nameInfoBox.MinimumSize = new System.Drawing.Size(150, 30);
-            this.nameInfoBox.Name = "nameInfoBox";
-            this.nameInfoBox.PlaceholderText = "\"Здесь должно прописываться имя пользователя\"";
-            this.nameInfoBox.ReadOnly = true;
-            this.nameInfoBox.Size = new System.Drawing.Size(446, 30);
-            this.nameInfoBox.TabIndex = 23;
-            this.nameInfoBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // panel3
             // 
@@ -205,16 +172,6 @@
             this.panel4.TabIndex = 2;
             this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("XO Thames", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(514, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(252, 42);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Список Услуг";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
@@ -226,6 +183,36 @@
             this.label3.Text = "Мне: добавить кнопку изменить услугу и кнопку добавить услугу";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("XO Thames", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(514, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(252, 42);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Список Услуг";
+            // 
+            // fullNameLabel
+            // 
+            this.fullNameLabel.AutoSize = true;
+            this.fullNameLabel.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.fullNameLabel.Location = new System.Drawing.Point(473, 19);
+            this.fullNameLabel.Name = "fullNameLabel";
+            this.fullNameLabel.Size = new System.Drawing.Size(293, 25);
+            this.fullNameLabel.TabIndex = 27;
+            this.fullNameLabel.Text = "\"Имя Фамилия Отчество\"";
+            // 
+            // roleLabel
+            // 
+            this.roleLabel.AutoSize = true;
+            this.roleLabel.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.roleLabel.Location = new System.Drawing.Point(502, 56);
+            this.roleLabel.Name = "roleLabel";
+            this.roleLabel.Size = new System.Drawing.Size(235, 25);
+            this.roleLabel.TabIndex = 30;
+            this.roleLabel.Text = "\"Роль пользователя\"";
+            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
@@ -234,11 +221,11 @@
             this.ClientSize = new System.Drawing.Size(1280, 720);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.servicesPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form3";
             this.Text = "Страница Услуг";
-            this.panel1.ResumeLayout(false);
+            this.servicesPanel.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
@@ -252,20 +239,20 @@
 
         #endregion
 
-        private Panel panel1;
+        private Panel servicesPanel;
         private Button button2;
         private Button button1;
         private Panel panel2;
         private Panel panel3;
         private Label label1;
         private PictureBox pictureBox1;
-        private TextBox nameInfoBox;
-        private TextBox textBox1;
         private Button offButton;
         private Button profileButton;
         private Panel panel4;
         private Label label2;
         private Button button3;
         private Label label3;
+        private Label fullNameLabel;
+        private Label roleLabel;
     }
 }
---
+++ sanatoriy/sanatoriy/ServiceForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace sanatoriy
+{
+    partial class ServiceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ServiceForm));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.roleLabel = new System.Windows.Forms.Label();
+            this.fullNameLabel = new System.Windows.Forms.Label();
+            this.profileButton = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.goServiceButton = new System.Windows.Forms.Button();
+            this.offButton = new System.Windows.Forms.Button();
+            this.datesFlowPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.roleLabel);
+            this.panel2.Controls.Add(this.fullNameLabel);
+            this.panel2.Controls.Add(this.profileButton);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.goServiceButton);
+            this.panel2.Controls.Add(this.offButton);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1280, 115);
+            this.panel2.TabIndex = 2;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // roleLabel
+            // 
+            this.roleLabel.AutoSize = true;
+            this.roleLabel.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.roleLabel.Location = new System.Drawing.Point(507, 56);
+            this.roleLabel.Name = "roleLabel";
+            this.roleLabel.Size = new System.Drawing.Size(235, 25);
+            this.roleLabel.TabIndex = 29;
+            this.roleLabel.Text = "\"Роль пользователя\"";
+            // 
+            // fullNameLabel
+            // 
+            this.fullNameLabel.AutoSize = true;
+            this.fullNameLabel.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.fullNameLabel.Location = new System.Drawing.Point(476, 19);
+            this.fullNameLabel.Name = "fullNameLabel";
+            this.fullNameLabel.Size = new System.Drawing.Size(293, 25);
+            this.fullNameLabel.TabIndex = 3;
+            this.fullNameLabel.Text = "\"Имя Фамилия Отчество\"";
+            // 
+            // profileButton
+            // 
+            this.profileButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(190)))), ((int)(((byte)(245)))));
+            this.profileButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.profileButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.profileButton.Font = new System.Drawing.Font("XO Thames", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.profileButton.Location = new System.Drawing.Point(843, 56);
+            this.profileButton.Name = "profileButton";
+            this.profileButton.Size = new System.Drawing.Size(436, 58);
+            this.profileButton.TabIndex = 26;
+            this.profileButton.Text = "Личный Кабинет";
+            this.profileButton.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(190)))), ((int)(((byte)(245)))));
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(400, 115);
+            this.panel3.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("XO Thames", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(149, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 43);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ОК \"Клей\"";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(115, 115);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // goServiceButton
+            // 
+            this.goServiceButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(190)))), ((int)(((byte)(245)))));
+            this.goServiceButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.goServiceButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.goServiceButton.Font = new System.Drawing.Font("XO Thames", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.goServiceButton.Location = new System.Drawing.Point(843, 0);
+            this.goServiceButton.Name = "goServiceButton";
+            this.goServiceButton.Size = new System.Drawing.Size(218, 57);
+            this.goServiceButton.TabIndex = 27;
+            this.goServiceButton.Text = "Услуги";
+            this.goServiceButton.UseVisualStyleBackColor = false;
+            // 
+            // offButton
+            // 
+            this.offButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(190)))), ((int)(((byte)(245)))));
+            this.offButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.offButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.offButton.Font = new System.Drawing.Font("XO Thames", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.offButton.Location = new System.Drawing.Point(1062, 0);
+            this.offButton.Name = "offButton";
+            this.offButton.Size = new System.Drawing.Size(218, 57);
+            this.offButton.TabIndex = 28;
+            this.offButton.Text = "Выход";
+            this.offButton.UseVisualStyleBackColor = false;
+            // 
+            // datesFlowPanel
+            // 
+            this.datesFlowPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.datesFlowPanel.Location = new System.Drawing.Point(0, 277);
+            this.datesFlowPanel.Name = "datesFlowPanel";
+            this.datesFlowPanel.Size = new System.Drawing.Size(1280, 411);
+            this.datesFlowPanel.TabIndex = 3;
+            // 
+            // ServiceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.datesFlowPanel);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ServiceForm";
+            this.Text = "ServiceForm";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel2;
+        private Button goServiceButton;
+        private Label fullNameLabel;
+        private Button profileButton;
+        private Panel panel3;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label roleLabel;
+        private Button offButton;
+        private FlowLayoutPanel datesFlowPanel;
+    }
+}
